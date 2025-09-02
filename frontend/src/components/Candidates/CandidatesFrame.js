fbfec0e2d2e732aeb33b535b4812ecda59f0ad50
nomish <nomish5057@gmail.com>
2025-09-02 14:10:48 +0300

    add basic Candidates Frame styling

--- frontend/src/components/Candidates/CandidatesFrame.js
+++ frontend/src/components/Candidates/CandidatesFrame.js
@@ -1,15 +1,22 @@
 import CandidatesButtons from "./CandidatesButtons";
 import CandidatesSearch from "./CandidatesSearch";
+import CandidatesList from "./CandidatesList";
+import './style/CandidatesFrame.scss';
 
 const CandidatesFrame = () => {
   return (
-    <div>
-      <h1>ניהול מועמדים</h1>
+    <div className="candidates-frame">
+      <h1 className="candidates-frame__title">ניהול מועמדים</h1>
       <CandidatesButtons
-        //onAdd={() => alert("Add Candidate clicked")}      
-        //onExport={() => alert("Export List clicked")}
-      />      
-      <CandidatesSearch />
+          //onAdd={() => alert("Add Candidate clicked")}      
+          //onExport={() => alert("Export List clicked")}
+        />   
+
+      <div className="candidates-frame__search-container">     
+        <CandidatesSearch />
+      </div>
+
+      <CandidatesList />
     </div>
 
   );
