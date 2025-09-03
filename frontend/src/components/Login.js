89cdc071f7cef454f2f8e916482cc09ad9961aca
hiEester <estersolo2584@gmail.com>
2025-09-03 15:50:48 +0300

    Login component is created

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Login(props) {
+   
+  return (
+
+    <div>
+      <input type="text" placeholder="שם משתמש" />
+      <br />
+      <input type="text" placeholder="סיסמה" />
+      <br />
+      <button >הכנס</button>
+      <br />
+      <a  href="#">Forgot Password?</a>
+      <br />
+      <a  href="#">Sign Up</a>
+    </div>
+  )
+}
